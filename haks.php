588930f8f98a831576088dce3edea968001a5490
Anatoly <politsin@gmail.com>
2017-04-04 09:42:21 +0300

    Update haks.php

--- haks.php
+++ haks.php
@@ -11,3 +11,7 @@
     stream_context_set_option($this->context, $options);
     ...
   }
+
+
+  // Allow other modules to alter the protections applied to this form.
+  \Drupal::moduleHandler()->alter('honeypot_form_protections', $options, $form);
