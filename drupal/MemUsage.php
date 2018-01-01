694f68830147db6600d9f0f949fe79064b73228c
Anatoly <politsin@gmail.com>
2018-01-01 18:16:02 +0300

    Update MemUsage.php

--- drupal/MemUsage.php
+++ drupal/MemUsage.php
@@ -1,18 +1,19 @@
 <?php
 
-  /**
-   * Bytes Formater.
-   */
-  public static function mem($messg, $precision = 2) {
-    $bytes = memory_get_peak_usage();
-    $units = array("b", "kb", "mb", "gb", "tb");
+/**
+ * Mem Usage.
+ */
+public static function mem($messg, $precision = 2) {
+  $bytes = memory_get_peak_usage();
+  $units = array("b", "kb", "mb", "gb", "tb");
 
-    $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-    $pow = min($pow, count($units) - 1);
+  $bytes = max($bytes, 0);
+  $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+  $pow = min($pow, count($units) - 1);
 
-    $bytes /= (1 << (10 * $pow));
+  $bytes /= (1 << (10 * $pow));
 
-    $result = round($bytes, $precision) . " " . $units[$pow];
-    dsm("$result - $messg");
-  }
+  $result = round($bytes, $precision) . " " . $units[$pow];
+  dsm("$result - $messg");
+}
+
