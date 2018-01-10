94059a28eb335f8de4193141d68d83804de58455
Anatoly <politsin@gmail.com>
2018-01-10 16:29:24 +0300

    Update update.sh

--- sh/update.sh
+++ sh/update.sh
@@ -22,7 +22,6 @@ docker-compose --version
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-docker-compose migrate-to-labels
 
 # reboot
 reboot
