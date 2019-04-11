e63dce2eb734d68f8b9b177818ac5372e3bf27e4
Anatoly <politsin@gmail.com>
2019-04-11 10:16:37 +0300

    Update form.php

--- drupal/form.php
+++ drupal/form.php
@@ -1,3 +1,61 @@
+```yaml
+app.settings:
+  path: '/admin/config/system/app'
+  defaults:
+    _form: '\Drupal\app\Form\Settings'
+    _title: 'App Settings'
+  requirements:
+    _permission: 'administer site configuration'
+```
+<?php
+namespace Drupal\app\Form;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+class Settings extends ConfigFormBase {
+    /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'app_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['app.settings'];
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('app.settings');
+    $form['general'] = [
+      '#type' => 'details',
+      '#title' => $this->t('General settings'),
+      '#open' => TRUE,
+    ];
+    $form['general']['foo'] = [
+      '#title' => $this->t('Foo'),
+      '#default_value' => $config->get('foo'),
+      '#type' => 'textfield',
+      '#description' => '',
+    ];
+  }
+  
+  /**
+   * Implements a form submit handler.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('app.settings');
+    $config
+      ->set('foo', $form_state->getValue('foo'))
+      ->save();
+  }
+
+}
+?>
+
 <?php
 
 namespace Drupal\app\Form;
