82dbae4ce6ee194433f711aa81b0ead338ff0f37
Anatoly <politsin@gmail.com>
2019-09-19 10:46:51 +0300

    Update EntityQuery.php

--- drupal/EntityQuery.php
+++ drupal/EntityQuery.php
@@ -2,6 +2,51 @@
 use Drupal\node\Entity\Node;
 use Drupal\taxonomy\Entity\Term;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+
+  /**
+   * The cml storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $cmlStorage;
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * Constructs a new CheckAuth object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity Manager service.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   Entity Repository service.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    EntityRepositoryInterface $entity_repository
+  ) {
+    $this->cmlStorage = $entity_type_manager->getStorage('cml');
+    $this->entityRepository = $entity_repository;
+  }
+
+/** ******************************/
+// Storage из EntityTypeManager.
+$query = $this->cmlStorage->getQuery()
+      ->condition('status', 1)
+      ->sort('created', $sort);
+$cml = $this->cmlStorage->create([
+      'name' => "name",
+])->save();
+// loadEntityByUuid.
+$cml = $this->entityRepository->loadEntityByUuid('cml', $cookie);
+
+
 /**
  * Query.
  */
